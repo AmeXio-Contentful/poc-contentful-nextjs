ee87c5c8bc1caa656ba12ce84592d68852b5266d
Celien Neven <celienneven@Celiens-MacBook-Pro.local>
2024-04-26 17:19:02 +0200

    component-styles improve icons

--- src/components/features/ctf-components/ctf-teaser-component/ctf-teaser-component.tsx
+++ src/components/features/ctf-components/ctf-teaser-component/ctf-teaser-component.tsx
@@ -8,14 +8,17 @@ interface Teaser {
 export const CtfTeaserComponent = (props: Teaser) => {
   const { image, body } = props;
   return (
-    <div className="md:px-48z h-auto px-6 pb-14 pt-9">
+    <div className="md:px-48z h-auto px-6 pb-14 pt-9 text-base">
+      <div className="relative max-w-full max-h-full overflow-hidden
+      mx-auto rounded-full m-auto w-28 h-28 bg-blue-900">
       <img
-        className="mx-auto rounded-full pb-6 m-auto"
+        className="min-w-full min-h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
         src={image?.url}
-        width="150px"
-        height="150px"
+        width="auto"
+        height="auto"
         alt="alt"
       />
+      </div>
       <div className="pt-5">
         <div className="text-s text-center">
           {documentToReactComponents(body?.json)}
