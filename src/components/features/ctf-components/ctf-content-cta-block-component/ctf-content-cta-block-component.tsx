474c57a616551a6cdc4d7571ef1dc839432c17d7
Celien Neven <celienneven@Celiens-MacBook-Pro.local>
2024-04-26 16:46:19 +0200

    component-styles fix alignment in content-block

--- src/components/features/ctf-components/ctf-content-cta-block-component/ctf-content-cta-block-component.tsx
+++ src/components/features/ctf-components/ctf-content-cta-block-component/ctf-content-cta-block-component.tsx
@@ -18,17 +18,19 @@ interface ContentCtaBlock {
 export default function CtfContentCtaBlockComponent(props: ContentCtaBlock) {
   const { buttonText, buttonLink, blockImage, blockBody, blockTitle } = props;
   return (
-    <div className="flex w-full max-w-[455px] flex-row gap-x-[75px]">
+    <div className="flex w-full max-w-[455px] flex-row gap-x-[75px] h-full">
       <img
         width="75px"
         className="place-self-start"
         alt="icon"
         src={blockImage?.url}
       />
-      <div>
-        <h3 className="text-2xl text-blue-900">{blockTitle}</h3>
-        <div className="my-3 h-auto">
-          {documentToReactComponents(blockBody.json)}
+      <div className="flex flex-col items-start justify-between">
+        <div>
+          <h3 className="text-2xl text-blue-900 font-medium">{blockTitle}</h3>
+          <div className="my-3 h-auto text-base">
+            {documentToReactComponents(blockBody.json)}
+          </div>
         </div>
         <button className="mt-6 bg-orange-500 px-5 py-3.5 text-sm text-white">
           {buttonText}
