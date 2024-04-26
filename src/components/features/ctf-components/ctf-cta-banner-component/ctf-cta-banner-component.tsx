aeb037cf8cdf44b898d4424ca67d8dbb5f4050e4
Celien Neven <celienneven@Celiens-MacBook-Pro.local>
2024-04-26 09:38:30 +0200

    component-styles add padding to hero banner

--- src/components/features/ctf-components/ctf-cta-banner-component/ctf-cta-banner-component.tsx
+++ src/components/features/ctf-components/ctf-cta-banner-component/ctf-cta-banner-component.tsx
@@ -12,7 +12,7 @@ export const CtfCtaBannerComponent = (
 
   return (
     <div
-      className="md:px-48z h-auto w-full px-6 pb-14 pt-9 bg-no-repeat bg-cover bg-center"
+      className="md:px-48z h-auto w-full px-16 pb-24 pt-36 bg-no-repeat bg-cover bg-center"
       style={{ backgroundImage: `url(${backgroundImage?.url})` }}>
       <div className="w-full lg:w-1/2">
         <p className="text-4xl font-semibold text-white">{title}</p>
