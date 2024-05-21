de0bfe5e5461c64e6d8668fe2e29f9230e38d9e0
HAKOBYAN, Davit <davit.hakobyan@acolad.com>
2024-05-21 11:18:59 +0200

    Refactor footer component

--- src/components/features/ctf-components/ctf-footer/ctf-footer.tsx
+++ src/components/features/ctf-components/ctf-footer/ctf-footer.tsx
@@ -1,27 +1,39 @@
-import { useContentfulInspectorMode } from '@contentful/live-preview/react';
-import Facebook from '@mui/icons-material/Facebook';
-import Instagram from '@mui/icons-material/Instagram';
-import LinkedIn from '@mui/icons-material/LinkedIn';
-import Twitter from '@mui/icons-material/Twitter';
-import { useTranslation } from 'next-i18next';
-
 import { FooterFieldsFragment } from './__generated/ctf-footer.generated';
-import styles from './ctf-footer.module.scss'
 
 import {
   getLinkDisplayText,
   getLinkHrefPrefix,
 } from '@src/components/features/ctf-components/ctf-navigation/utils';
+import {
+  DoormatComponent,
+  DoormatItem,
+  DoormatItemChild, DoormatProps,
+} from '@src/components/features/decoupled-components/doormat/doormat';
 import { FooterComponent, FooterProps } from '@src/components/features/decoupled-components/footer/footer';
-import { Link } from '@src/components/shared/link';
-import { useContentfulContext } from '@src/contentful-context';
 
 export const CtfFooter = (props: FooterFieldsFragment) => {
   const footerContent = props.items[0];
 
-  const { t } = useTranslation();
-  const { locale } = useContentfulContext();
-  const inspectorMode = useContentfulInspectorMode();
+  const doormatProps: DoormatProps = {items: []};
+  if(footerContent && footerContent.menuItemsCollection){
+    doormatProps.items =  footerContent.menuItemsCollection.items.map((collection)=>{
+
+      const doormatItem: DoormatItem = {
+        title: collection?.groupName || '',
+        imageUrl: collection?.footerImage?.url || '',
+        children: collection?.featuredPagesCollection?.items.map((collectionChild)=>{
+          const doormatItemChild: DoormatItemChild = {
+            title: collectionChild?.pageName || '',
+            url: getLinkHrefPrefix(collectionChild)
+          }
+          return doormatItemChild;
+        }) || []
+      }
+      return doormatItem;
+    });
+  }
+
+
 
   const footerProps: FooterProps = {};
   footerProps.legalLinks = footerContent?.legalLinks?.featuredPagesCollection?.items?.map(item => ({
@@ -38,50 +50,7 @@ export const CtfFooter = (props: FooterFieldsFragment) => {
   }
   return (
     <>
-      <div className={styles.doormat_container}>
-        <div className={styles.doormat}>
-          {footerContent && footerContent.menuItemsCollection && footerContent.menuItemsCollection.items.map(
-            menuItem =>
-              menuItem && (
-                <div className={styles.doormat__column} key={menuItem.sys.id}>
-                  <img className="text-white" src={menuItem.footerImage?.url as string}
-                       alt={menuItem.footerImage?.title as string}
-                  />
-                  <h3
-                    {...inspectorMode({
-                      entryId: menuItem.sys.id,
-                      fieldId: 'groupName',
-                      locale,
-                    })}
-                  >
-                    {menuItem.groupName}
-                  </h3>
-                  {menuItem.featuredPagesCollection && (
-                    <ul>
-                      {menuItem.featuredPagesCollection.items.map((item)=>{
-                        if(item){
-                          return (
-                            <li
-                              key={item.sys.id}
-                              {...inspectorMode({
-                                entryId: item.sys.id,
-                                fieldId: 'pageName',
-                              })}
-                            >
-                              <Link href={getLinkHrefPrefix(item)} title={getLinkDisplayText(item)}>
-                                {getLinkDisplayText(item)}
-                              </Link>
-                            </li>
-                          )
-                        }
-                      })}
-                    </ul>
-                  )}
-                </div>
-              ),
-          )}
-        </div>
-      </div>
+      <DoormatComponent items={doormatProps.items} />
       <FooterComponent legalLinks={footerProps.legalLinks} socials={footerProps.socials}  />
     </>
   );
---
+++ src/components/features/decoupled-components/doormat/doormat.tsx
@@ -0,0 +1,54 @@
+import styles from '@src/components/features/decoupled-components/doormat/doormat.module.scss'
+import { Link } from '@src/components/shared/link';
+
+export interface DoormatProps {
+  items?: DoormatItem[]
+}
+
+export interface DoormatItem {
+  title: string,
+  children: DoormatItemChild[],
+  imageUrl: string,
+}
+
+export interface DoormatItemChild {
+  title: string,
+  url: string,
+}
+export const DoormatComponent = (props: DoormatProps) => {
+  return (
+    <div className={styles.doormat_container}>
+      <div className={styles.doormat}>
+        {props.items && props.items.map(
+          (menuItem, index) =>
+            menuItem && (
+              <div className={styles.doormat__column} key={index}>
+                <img className="text-white" src={menuItem.imageUrl}
+                     alt={menuItem.title}
+                />
+                <h3>
+                  {menuItem.title}
+                </h3>
+                {menuItem.children && (
+                  <ul>
+                    {menuItem.children.map((item) => {
+                      if (item) {
+                        return (
+                          <li
+                          >
+                            <Link href={item.url} title={item.title}>
+                              {item.title}
+                            </Link>
+                          </li>
+                        )
+                      }
+                    })}
+                  </ul>
+                )}
+              </div>
+            ),
+        )}
+      </div>
+    </div>
+  )
+}
