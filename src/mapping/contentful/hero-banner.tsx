61fdf3cbcc3a3fd775dd5c91dd3f3f9f1833258e
Celien Neven <celienneven@Celiens-MacBook-Pro.local>
2024-05-27 16:18:50 +0200

    feature/separation-of-concerns remove duplicate code, reuse component

--- src/mapping/contentful/hero-banner.tsx
+++ src/mapping/contentful/hero-banner.tsx
@@ -7,41 +7,9 @@ import {TitleProps} from '@src/components/features/decoupled-components/title/ti
 import {
   ComponentHeroBannerFragment
 } from '@src/data/contentful/hero-banner/__generated/hero-banner.generated';
-import {HeroBanner} from '@src/components/features/decoupled-components/hero-banner';
-
-
-// would it be a good idea to use a state and manage that state through the mapping, then?
-// wouldn't this be overkill, since it would mostly be static data? is there another way to easily decouple the data from the components?
-// how about ssg? will this work?
-/* export const HeroBanner = (props: ComponentHeroBannerFragment) => {
-  const { backgroundImage , title, subtitle, buttonText, buttonUrl} = props;
-  const titleProps: TitleProps = {
-    title: title as string,
-    heading: 'h1'
-  };
-
-  const descriptionProps: DescriptionProps = {
-    description: subtitle as string,
-    descriptionClasses: 'mt-6 mb-5 text-xl text-white max-w-xl'
-  }
-
-  const buttonProps: ButtonProps = {
-    buttonText: buttonText as string,
-    buttonUrl: buttonUrl as string,
-    // buttonClasses: styles.hero_button // 'mt-6 bg-blue-500 px-5 py-3.5 text-base text-white'
-  }
-
-  return (
-    <div className={styles.hero_banner}
-         style={{ backgroundImage: `url(${backgroundImage?.url})` }}
-    >
-      <Teaser
-        titleProps={titleProps}
-        descriptionProps={descriptionProps}
-        buttonProps={buttonProps}
-      />
-    </div>
-  )
-}; */
+import {HeroBanner} from '@src/components/features/decoupled-components/hero-banner/hero-banner';
 
 console.log("is this mapping actually used?");
+export const HeroBannerComponent = (props: ComponentHeroBannerFragment) => {
+  return (<HeroBanner {...props} />)
+};
