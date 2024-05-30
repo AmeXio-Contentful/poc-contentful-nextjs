1f9b7ba90819942d8f9e033aa450698c8e652eed
Celien Neven <celienneven@Celiens-MacBook-Pro.local>
2024-05-30 10:40:15 +0200

    feature/styling adjust comments

--- src/components/features/decoupled-components/button/button.tsx
+++ src/components/features/decoupled-components/button/button.tsx
@@ -5,11 +5,7 @@ export const Button = (props: ButtonProps) => {
   const { buttonUrl, buttonText, buttonClasses } = props;
 
   return (
-    // there is a problem loading multiple classes, especially if the aim is to override certain properties
-    // it isn't predictable which properties will be loaded first, therefor you don't have full control over the styling this way
-    // variables can't be edited from outside of this module, since it is fully encapsulated
-    // it might be possible to add a variations through classes instead of overriding all properties by adding a class
-    // needs further investigation on how to approach this problem
+    // see description example on how to use variations instead of passing a class, this might result in unpredictable behaviour in styling
       <button className={`${styles.button} ${buttonClasses || ''}`}>
         <a href={buttonUrl as string}> {buttonText} </a>
       </button>
--- src/components/features/decoupled-components/title/title.tsx
+++ src/components/features/decoupled-components/title/title.tsx
@@ -12,11 +12,7 @@ export const Title = (props: TitleProps) => {
     title,
   );
   return (
-    // there is a problem loading multiple classes, especially if the aim is to override certain properties
-    // it isn't predictable which properties will be loaded first, therefor you don't have full control over the styling this way
-    // variables can't be edited from outside of this module, since it is fully encapsulated
-    // it might be possible to add a variations through classes instead of overriding all properties by adding a class
-    // needs further investigation on how to approach this problem
+    // see description example on how to use variations instead of passing a class, this might result in unpredictable behaviour in styling
     <div className={`${styles.title} ${titleClasses || ''}`}>
         { headingElement }
     </div>
