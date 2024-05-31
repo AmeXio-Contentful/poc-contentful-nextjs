425656899c9892dbae7f20882586a3fd165e6cc6
Celien Neven <celienneven@Celiens-MacBook-Pro.local>
2024-05-31 10:13:05 +0200

    feature/decoupled fix import

--- src/data/contentful/solutions/solutions-gql.tsx
+++ src/data/contentful/solutions/solutions-gql.tsx
@@ -1,5 +1,5 @@
 import {DefaultQueryParameters} from '@src/data/contentful/component.typings';
-import {useSolutionSectionComponentQuery} from '@src/data/contentful/solutions/__generated/solution.generated';
+import {useSolutionSectionComponentQuery} from '@src/data/contentful/solutions/__generated/solutions.generated';
 
 export function SolutionsGql({
   id,
